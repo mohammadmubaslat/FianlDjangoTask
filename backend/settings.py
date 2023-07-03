e891a52cec91102960e82ea85e3e660d5f3d76ac
mohammadmubaslat <mubaslat2044@gmail.com>
2023-07-03 16:12:16 +0300

    postgres added

--- backend/settings.py
+++ backend/settings.py
@@ -87,10 +87,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-       
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'de123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
